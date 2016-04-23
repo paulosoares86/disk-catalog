3a066ba0eaac838ad5542c7218098fef4f210b45
Paulo <phsoares.ita@gmail.com>
2016-04-23 11:03:58 -0300

    hiding pagination if there are no elements

--- frontend/app/components/pagination.component.ts
+++ frontend/app/components/pagination.component.ts
@@ -9,7 +9,7 @@ import {Input, Component} from 'angular2/core';
       }
     `],
     template: `
-      <nav class="pagination-nav">
+      <nav *ngIf="totalPages" class="pagination-nav">
         <ul class="pagination">
 
           <li class="page-item" [class.disabled]="currentPage == 1">
